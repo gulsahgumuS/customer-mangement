5e614ed6c19eac97a608f3ad75f0611a06b4e969
gulsah <gulsahgumuss422@gmail.com>
2024-10-28 18:28:31 +0300

    asdasd

--- customers.py
+++ customers.py
@@ -1,17 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
-from models import db, Customer  # models.py dosyasını içe aktar
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db.init_app(app)
+db = SQLAlchemy(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()  # Tabloları oluştur
-
+class Customer(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String, nullable=False)
+    addr = db.Column(db.String)
+    city = db.Column(db.String)
+    pin = db.Column(db.String)
+    phone = db.Column(db.String)
 
 @app.route('/')
 def home():
@@ -29,22 +31,19 @@ def addrec():
             addr = request.form['add']
             city = request.form['city']
             pin = request.form['pin']
-            phone = request.form['phone']
 
-            new_customer = Customer(name=nm, addr=addr, city=city, pin=pin, phone=phone)
+            new_customer = Customer(name=nm, addr=addr, city=city, pin=pin)
             db.session.add(new_customer)
             db.session.commit()
-            msg = "Record successfully added"
+            return redirect(url_for('list_customer'))
         except Exception as e:
             db.session.rollback()
-            msg = f"Error in insert operation: {e}"
-        finally:
-            return render_template("result.html", msg=msg)
+            return redirect(url_for('list_customer'))
 
 @app.route('/list')
-def list_customers():
-    customers = Customer.query.all()  # Tüm müşterileri al
-    return render_template("list.html", rows=customers)
+def list_customer():
+    customer = Customer.query.all()
+    return render_template("list.html", rows=customer)
 
 if __name__ == '__main__':
     app.run(debug=True)
